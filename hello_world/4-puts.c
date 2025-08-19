a6a97101bf6aaacc23a7baf2af48ba0dd60f03bd
gulay-yusifli <yusifligulay06@gmail.com>
2025-08-19 19:26:37 +0000

    task

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a specific sentence using puts
+ *
+ * Return:  Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
