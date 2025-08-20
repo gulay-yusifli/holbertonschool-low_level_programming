7ecbc6673bb010f79eeb0bc08cfbd16675019cda
gulay-yusifli <yusifligulay06@gmail.com>
2025-08-20 11:05:54 +0000

    task

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 	{
-		printf("is positive\n")
+		printf("is positive\n");
 	}
+	else if (n == 0)
+	{
+		printf("is zero");
+	}
+	else
+	{
+		printf("is negative");
+	}	
 	return (0);
 }
