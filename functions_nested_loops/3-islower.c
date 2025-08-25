25c74dd40f4d388509dd6c7b3d7889f8622720ce
gulay-yusifli <yusifligulay06@gmail.com>
2025-08-25 10:35:40 +0000

    task

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 /**
  * _islower - checks if a character is lowercase
- * @c: the character to check
+ * @word: the character to check
  *
  * Return: 1 if lowercase, 0 otherwise
  */
