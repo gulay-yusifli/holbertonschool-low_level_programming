619ec2c48a89b7d6013707f16e0055ce9e4e8973
gulay-yusifli <yusifligulay06@gmail.com>
2025-09-10 10:24:26 +0000

    task

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int dg = 0;
+
+	while (dg <= 9)
+	{
+		_putchar(dg + '0');
+		dg++;
+	}
+	_putchar('\n');
+}
