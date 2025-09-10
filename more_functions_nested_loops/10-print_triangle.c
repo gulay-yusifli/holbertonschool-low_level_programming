4896ba9a121f6e27d444a30a4b737c8da60b5ff4
gulay-yusifli <yusifligulay06@gmail.com>
2025-09-10 10:34:24 +0000

    task

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-aligned triangle of '#' characters
+ * @size: size of the triangle
+ *
+ * If size is 0 or less, prints only a newline.
+ */
+
+void print_triangle(int size)
+{
+	int row, htag;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		row = 1;
+
+		while (row <= size)
+		{
+			htag = 1;
+
+			while (htag <= size - row)
+			{
+				_putchar(' ');
+				htag++;
+			}
+			htag = 1;
+			while (htag <= row)
+			{
+				_putchar('#');
+				htag++;
+			}
+			_putchar('\n');
+			row++;
+		}
+	}
+}
