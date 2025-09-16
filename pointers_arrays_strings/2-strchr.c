946e70a76e3c2ceca546f65990d84db384bce115
gulay-yusifli <yusifligulay06@gmail.com>
2025-09-16 19:48:35 +0000

    task

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - check the code
+ * @s: source
+ * @c: character
+ *
+ * Return:  char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
